f9c4277321aac13fb8ee5280ab5573fa084287d8
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-13 18:10:42 -0800

    2 задание

--- fajr/src/sulik1/Main.java
+++ fajr/src/sulik1/Main.java
@@ -1,4 +1,10 @@
 package sulik1;
 
 public class Main {
+    public static void main(String[] args) {
+    int a=2;
+        System.out.println("s="+a*a);
+
+    }
+
 }
---
+++ fajr/src/sulik2/Main1.java
@@ -0,0 +1,4 @@
+package sulik2;
+
+public class Main1 {
+}
