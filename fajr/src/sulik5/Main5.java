6d4257db19371f6b7afdbf1c47728adc0edb9ca5
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-16 15:03:58 -0800

    5 домашка

---
+++ fajr/src/sulik5/Main5.java
@@ -0,0 +1,11 @@
+package sulik5;
+
+public class Main5 {
+    public static void main(String[] args) {
+   int a=4;
+        System.out.println("V+"+a*a*a);
+        int b=6;
+        System.out.println("S="+b*Math.pow(a, 2));
+
+    }
+}
