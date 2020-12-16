f39151f69fbc9bdc432d9739d28661a5946704f5
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-16 15:47:56 -0800

    6 домашка

---
+++ fajr/src/sulik6/Main6.java
@@ -0,0 +1,13 @@
+package sulik6;
+
+public class Main6 {
+    public static void main(String[] args) {
+     int a=2;
+     int b=3;
+     int c=4;
+        System.out.println("V="+a*b*c);
+        System.out.println("S="+2*(a*b+b*c+a*c));
+
+    }
+
+}
