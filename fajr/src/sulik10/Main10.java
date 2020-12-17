10d220ea06755ed10a57fae1cee8a62301401002
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-16 19:06:43 -0800

    9 домашка

---
+++ fajr/src/sulik10/Main10.java
@@ -0,0 +1,11 @@
+package sulik10;
+
+public class Main10 {
+    public static void main(String[] args) {
+        int a=4;
+        int b=16;
+        System.out.println("b-a=c"+"b+a=c"+"b/a=c"+"b*a=c");
+
+
+    }
+}
