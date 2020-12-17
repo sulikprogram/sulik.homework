d23690e9a73b53128be509a0af7675ccfba2174a
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-16 18:46:27 -0800

    8 домашка

---
+++ fajr/src/sulik9/Main9.java
@@ -0,0 +1,14 @@
+package sulik9;
+
+public class Main9 {
+    public static void main(String[] args) {
+        int a=7;
+        int b=7;
+        System.out.println("Math.sqrt(a*b)");
+
+
+
+    }
+
+
+}
