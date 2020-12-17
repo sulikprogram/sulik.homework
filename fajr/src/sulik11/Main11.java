30d0dcd40aca565ec8a1006614eddfd388d7d674
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-16 19:16:36 -0800

    11 домашка

---
+++ fajr/src/sulik11/Main11.java
@@ -0,0 +1,12 @@
+package sulik11;
+
+public class Main11 {
+    public static void main(String[] args) {
+        int a=4;
+        int b=2;
+        System.out.println("a+b=c"+"a-b=c"+"a/b=c"+"a/b=c");
+
+
+    }
+}
+
