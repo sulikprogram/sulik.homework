c7dc92c4bcd84f3841960c5ca6d05762638a7ae6
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-16 18:09:42 -0800

    7 домашка

---
+++ fajr/src/sulik7/Main7.java
@@ -0,0 +1,10 @@
+package sulik7;
+
+public class Main7 {
+    public static void main(String[] args) {
+     int R=5;
+     double p=3.14;
+        System.out.println("L="+2*p*R);
+        System.out.println("S="+p*Math.pow(R, 2));
+    }
+}
