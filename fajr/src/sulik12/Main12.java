14bf585a58f5ed1ad01963cd97c3e2553106b505
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-16 19:59:43 -0800

    12 домашка

---
+++ fajr/src/sulik12/Main12.java
@@ -0,0 +1,14 @@
+package sulik12;
+
+public class Main12 {
+    public static void main(String[] args) {
+        short a = 4;
+        short b =4;
+        short n =4;
+        short m = 12;
+        short g = 5;
+        System.out.println("P="+ m);
+        System.out.println("C= "+ n);
+
+    }
+}
