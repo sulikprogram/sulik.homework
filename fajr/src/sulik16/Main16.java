8b7a0c763b9f89cf785c21c48489903ec7947040
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-22 12:05:31 -0800

    задача 16

---
+++ fajr/src/sulik16/Main16.java
@@ -0,0 +1,11 @@
+package sulik16;
+
+public class Main16 {
+    public static void main(String[] args){
+        byte a=5;
+        byte d=1;
+        byte t=5+1;
+        System.out.println("T="+t);
+    }
+
+}
